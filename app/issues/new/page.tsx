def20739f874240449760049bdaffeabc328d591
JulieBaculio <baculio.julieanne23@gmail.com>
2024-03-26 16:45:08 +0800

    successful db insert

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMde from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
+const NewIssuePage = () => {
+  const router = useRouter();
+  //register to track the form
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  return (
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMde placeholder="Description" {...field} />
+        )}
+      />
+      <Button>Submit New Issue</Button>
+    </form>
+  );
+};
+
+export default NewIssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>
+        <NavBar />
+        <Theme>
+          <main className="p-5">{children}</main>
+          {/*<ThemePanel></ThemePanel>*/}
+        </Theme>
+      </body>
     </html>
   );
 }
